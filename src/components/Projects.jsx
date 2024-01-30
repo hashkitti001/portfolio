e86bd8d7734b9065549e6be259bac6bac9871b61
Iwegbu Jeddy <76275625+hashkitti001@users.noreply.github.com>
2024-01-30 21:38:41 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -41,7 +41,7 @@ const Projects = () => {
         </div>
        
          <div className="portfolio_item">
-          <img className="project-img" src="../assets/chatapp.jpg" alt="" />
+          <img className="project-img" src="https://i.imgur.com/FyNjDkK.jpg" alt="Chat app" />
           <h3>Chat App </h3>
           <div className="project-buttons">
             <a className="btn" href="https://github.com/hashkitti001/chatapp">Repo Link</a>
